039b9678e4c9e86499f6779c4c3b60e940a78c0b
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2024-12-14 15:41:07 +0530

    Add files via upload

---
+++ tailwind.config.mjs
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: '#0d6efd', // Adjust based on branding
+        secondary: '#6c757d',
+        accent: '#20c997',
+        light: '#f8f9fa',
+        dark: '#121212',
+      },
+      animation: {
+        'spin-slow': 'spin 6s linear infinite',
+        'fade-in': 'fadeIn 1s ease-out',
+        scroll: 'scroll 15s linear infinite',
+        fadeIn: "fadeIn 1s ease-in-out forwards",
+      },
+      keyframes: {
+        scroll: {
+            '0%': { transform: 'translateX(0)' },
+            '100%': { transform: 'translateX(-100%)' },
+        },
+        fadeIn: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+    },
+    },
+  },
+  plugins: [],
+};
