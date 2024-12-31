49b674b00be5297a3508e7ccc4f7bc5abec7294b
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2024-12-31 15:21:50 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -8,16 +8,10 @@ import AboutUs from '../components/AboutUs';
 import Contact from '../components/Contact';
 import Testimonial from '../components/Testimonial';
 import Footer from '../components/Footer';
-import Head from 'next/head';
 
 export default function Home() {
     return (
         <>
-        <Head>
-         <meta name="google-adsense-account" content="ca-pub-8553283570584154" />
-        {/* Google Adsense Script Tag */}
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8553283570584154" crossOrigin="anonymous"></script>
-        </Head>
             <Navbar />
             <Hero />
             <MarqueeComponent />
