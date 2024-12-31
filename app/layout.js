f2a31aea432113b3cf6e49c623e5d002a2edb62d
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2024-12-31 15:04:50 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <meta name="google-adsense-account" content="ca-pub-8553283570584154">
+    <Head>
+        {/* Place the meta tag inside Head */}
+        <meta name="google-adsense-account" content="ca-pub-8553283570584154" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
